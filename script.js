6fe4b1676a3f64e51790b00af69e96c743b83aa9
NGAIPERA <prudentngaipera4@gmail.com>
2023-08-27 15:10:19 +0200

    ni

---
+++ script.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+        question: "Qu'est-ce que HTML signifie?",
+        choices: ["Hyper Text Markup Language", "High Tech Modern Language", "Hyper Transfer Markup Language"],
+        correctAnswer: 0
+    },
+    {
+        question: "Quel langage de programmation est utilisé pour styliser les pages web?",
+        choices: ["Java", "CSS", "Python"],
+        correctAnswer: 1
+    },
+    {
+        question: "Quelle balise est utilisée pour lier un fichier JavaScript externe?",
+        choices: ["<js>", "<javascript>", "<script>"],
+        correctAnswer: 2
+    },
+    // ... (ajoutez les 37 questions supplémentaires ici)
+    {
+        question: "Quelle balise est utilisée pour créer un lien hypertexte?",
+        choices: ["<link>", "<a>", "<href>"],
+        correctAnswer: 1
+    },
+    {
+        question: "Quelle propriété CSS est utilisée pour définir la couleur du texte?",
+        choices: ["text-color", "color", "font-color"],
+        correctAnswer: 1
+    }
+];
+
+// Le reste du code JavaScript reste inchangé
